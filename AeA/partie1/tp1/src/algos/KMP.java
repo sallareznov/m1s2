a95f186c89ac62ad38f836c2d2aaef5c466d4af1
LeoPerard <leo.perard@gmail.com>
2015-01-16 11:59:12 +0100

    debut de l'implementation du motif et de son alphabet creation de la classe KMP (algo de recherche de motif)

---
+++ AeA/partie1/tp1/src/algos/KMP.java
@@ -0,0 +1,21 @@
+/**
+ * Algorithme de Knuth, Morris et Pratt pour la recherche de mots dans un texte
+ */
+package algos;
+
+/**
+ * @author leo
+ *
+ */
+public class KMP
+{	
+
+	/**
+	 * 
+	 */
+	public KMP()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
