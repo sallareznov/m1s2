8e481481ac433de7f10e58b7dea07029b5865547
LeoPerard <leo.perard@gmail.com>
2015-02-20 11:18:31 +0100

    updte the class Genome due to the previous changes on Strand now Genome inherits of Strand

--- AeA/partie1/tp1/src/patterns/Genome.java
+++ AeA/partie1/tp1/src/patterns/Genome.java
@@ -8,70 +8,47 @@ import bases.BaseFlyweightFactory;
 /**
  * Classe representant un genome
  */
-public class Genome {
-	
-	private Alphabet alphabet;
-	private Base[] bases;
-	private int size;
+public class Genome extends Strand
+{
+	/**
+	 * construit un genome
+	 * 
+	 * @param sequenceString
+	 *            le motif
+	 * @param alphabet
+	 *            l'alphabet
+	 */
+	public Genome(String sequenceString)
+	{
+		super(sequenceString);
+	}
 
 	/**
 	 * construit un genome
-	 * @param sequenceString le motif
-	 * @param alphabet l'alphabet
+	 * 
+	 * @param bases
+	 *            les bases du genome
+	 * @param alphabet
+	 *            l'alphabet
 	 */
-	public Genome(String sequenceString, Alphabet alphabet) {
-		this.alphabet = alphabet;
-		this.size = sequenceString.length();
-		final BaseFlyweightFactory baseFactory = new BaseFlyweightFactory(alphabet);
-		if (this.alphabet.acceptWord(sequenceString))
-			this.bases = baseFactory.createBases(sequenceString);
-		else
-			throw new IllegalArgumentException(
-					"String contains invalid letters for the alphabet");
+	public Genome(char[] bases)
+	{
+		super(bases);
 	}
-	
-	/**
-	 * construit un genome
-	 * @param bases les bases du genome
-	 * @param alphabet l'alphabet
-	 */
-	public Genome(Base[] bases, Alphabet alphabet) {
-		this.bases = bases;
-		this.alphabet = alphabet;
-		this.size = bases.length;
-	}
-	
-	/**
-	 * @return les bases du genome
-	 */
-	public Base[] getBases() {
-		return bases;
-	}
-	
-	/**
-	 * @return la taille du genome
-	 */
-	public int getSize() {
-		return size;
-	}
-	
-	/**
-	 * @return l'alphabet des bases du genome
-	 */
-	public Alphabet getAlphabet() {
-		return alphabet;
-	}
-	
+
 	@Override
-	public String toString() {
-		return new ConcreteStrand(bases).toString();
+	public String toString()
+	{
+		return new Strand(this.getContent()).toString();
 	}
-	
+
 	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof Genome) {
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof Genome)
+		{
 			final Genome genome = (Genome) obj;
-			return Arrays.equals(bases, genome.getBases());
+			return Arrays.equals(this.getContent(), genome.getContent());
 		}
 		return false;
 	}
