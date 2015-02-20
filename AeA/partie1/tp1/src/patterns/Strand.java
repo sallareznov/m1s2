26679cef275cff7311bafd4e75161486d659f311
LeoPerard <leo.perard@gmail.com>
2015-02-20 11:08:58 +0100

    refactor Strand - remove empty strand - Concrete Strand is now the unique strand - no more interface but one mother class

--- AeA/partie1/tp1/src/patterns/Strand.java
+++ AeA/partie1/tp1/src/patterns/Strand.java
@@ -1,92 +1,207 @@
 package patterns;
 
+import java.util.Arrays;
+
 import bases.Base;
+import bases.BaseFlyweightFactory;
+import bases.util.PairingManager;
 
-public interface Strand {
+/**
+ * Classe représentant un brin d'ADN
+ */
+public class Strand
+{
+	/**
+	 * Manager pour gere les paires
+	 */
+	private PairingManager manager;
 
-    /**
-     * @return le contenu du brin
-     */
-    Base[] getContent();
+	/**
+	 * Contenu du brin
+	 */
+	private char[] content;
 
-    /**
-     * retourne la base à l'indice <i>i</i>
-     * <i>i</i> est compris entre 0 et la taille du strand
-     * 
-     * @param index
-     *            l'indice de la base
-     * @return la base a l'indice <i>i</i>
-     */
-    Base getBaseAt(int index);
+	/**
+	 * construit le brin d'ADN
+	 * 
+	 * @param content
+	 *            le contenu du brin
+	 */
+	public Strand(char[] content)
+	{
+		this.setContent(content);
+	}
 
-    /**
-     * @return la taille du brin
-     */
-    int getSize();
+	/**
+	 * construit le brin d'ADN
+	 * 
+	 * @param word
+	 *            la chaine representant le contenu du brin
+	 */
+	public Strand(String word)
+	{
+		this.setContent(word);
+	}
 
-    /**
-     * @return le brin reverse
-     */
-    Strand getReverse();
+	/**
+	 * @return le contenu du brin
+	 */
+	public char[] getContent()
+	{
+		return this.content;
+	}
 
-    /**
-     * @return le brin complementaire
-     */
-    Strand getComplementary();
+	public void setContent(char[] content)
+	{
+		this.content = content;
+	}
 
-    /**
-     * @return le brin reverse-complementaire
-     */
-    Strand getReverseComplementary();
+	public void setContent(String word)
+	{
+		this.content = new char[word.length()];
+		for (int i = 0; i < this.content.length; i++)
+		{
+			this.content[i] = word.charAt(i);
+		}
+	}
 
-    /**
-     * @param size
-     *            la taille du brin préfixe recherché
-     * @return le brin préfixe
-     */
-    Strand getPrefix(int size);
+	/**
+	 * @return le manager des paires
+	 */
+	public PairingManager getManager()
+	{
+		return this.manager;
+	}
 
-    /**
-     * @param size
-     *            la taille du brin suffixe recherché
-     * @return le brin suffixe
-     */
-    Strand getSuffix(int size);
+	/**
+	 * @return la taille du brin
+	 */
+	public int getSize()
+	{
+		return this.getContent().length;
+	}
 
-    /**
-     * @return le plus long bord du brin
-     */
-    Strand getLongestEdge();
+	public Strand getReverse()
+	{
+		final int n = this.getSize();
+		final char[] reverseContent = new char[n];
+		for (int i = n - 1; i >= 0; i--)
+		{
+			reverseContent[i] = content[i];
+		}
+		return new Strand(reverseContent);
+	}
 
-    /**
-     * @param prefix
-     *            le brin teste en tant que prefix
-     * @return true si le brin est prefixe, faux sinon
-     */
-    boolean isPrefix(Strand prefix);
+	public Strand getComplementary()
+	{
+		final int n = this.getSize();
+		final char[] complementaryContent = new char[n];
+		for (int i = 0; i < n; i++)
+		{
+			complementaryContent[i] = this.getManager().getComplementary(
+					this.getBaseAt(i));
+		}
+		return new Strand(complementaryContent);
+	}
 
-    /**
-     * @param suffix
-     *            le brin teste en tant que suffixe
-     * @return true si le brin est suffixe, faux sinon
-     */
-    boolean isSuffix(Strand suffix);
+	public Strand getReverseComplementary()
+	{
+		final Strand reverseSubSequence = getReverse();
+		return reverseSubSequence.getComplementary();
+	}
 
-    /**
-     * Un brin est un bord d'un autre brin s'il est préfixe et suffixe en meme temps
-     * 
-     * @param preAndSuffix
-     *            le brin a testé si c'est un bord
-     * @return true si le brin est un bord, false sinon
-     */
-    boolean isEdge(Strand preAndSuffix);
+	public Strand getPrefix(int size)
+	{
+		if (size <= 0) { return new Strand(new char[0]); }
+		char[] prefixBases = new char[size];
+		for (int i = 0; i < prefixBases.length; i++)
+		{
+			prefixBases[i] = this.getBaseAt(i);
+		}
+		return new Strand(prefixBases);
+	}
 
-    @Override
-    String toString();
+	public Strand getSuffix(int size)
+	{
+		if (size <= 0) { return new Strand(new char[0]); }
+		char[] suffixBases = new char[size];
+		for (int i = this.getSize() - size; i < this.getSize(); i++)
+		{
+			suffixBases[i - (this.getSize() - size)] = this.getBaseAt(i);
+		}
+		return new Strand(suffixBases);
+	}
 
-    @Override
-    boolean equals(Object o);
+	public Strand getLongestEdge()
+	{
+		Strand edge = new Strand(new char[0]);
+		for (int i = 0; i <= this.getSize(); i++)
+		{
+			final Strand prefix = this.getPrefix(i);
+			final Strand suffix = this.getSuffix(i);
+			if (prefix.equals(suffix)) edge = prefix;
+		}
+		return edge;
+	}
 
-    Strand clone();
+	public boolean isPrefix(Strand prefix)
+	{
+		if (prefix.getSize() == 0) { return false; }
+		for (int i = 0; i < prefix.getSize(); i++)
+		{
+			if (!(prefix.getBaseAt(i) == (this.getBaseAt(i)))) return false;
+		}
+		return true;
+	}
+
+	public boolean isSuffix(Strand suffix)
+	{
+		if (suffix.getSize() == 0) { return false; }
+		for (int i = suffix.getSize() - 1; i >= 0; i--)
+		{
+			int index = i + (this.getSize() - suffix.getSize());
+			if (!(getBaseAt(index) == suffix.getBaseAt(i))) return false;
+		}
+		return true;
+	}
+
+	public boolean isEdge(Strand preAndSuffix)
+	{
+		return this.isPrefix(preAndSuffix) && this.isSuffix(preAndSuffix);
+	}
+
+	@Override
+	public String toString()
+	{
+		final StringBuilder stringBuilder = new StringBuilder();
+		for (char base : content)
+		{
+			stringBuilder.append(base);
+		}
+		return stringBuilder.toString();
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Strand other = (Strand) obj;
+		if (!Arrays.equals(content, other.content)) return false;
+		return true;
+	}
+
+	public char getBaseAt(int index)
+	{
+		return this.getContent()[index];
+	}
+
+	public Strand clone()
+	{
+		final char[] contentCopy = new char[content.length];
+		System.arraycopy(content, 0, contentCopy, 0, content.length);
+		return new Strand(contentCopy);
+	}
 
 }
--- AeA/partie1/tp1/test/patterns/StrandTest.java
+++ AeA/partie1/tp1/test/patterns/StrandTest.java
@@ -13,41 +13,47 @@ import bases.BaseFlyweightFactory;
 /**
  * Classe de test de la classe Strand
  */
-public class StrandTest {
+public class StrandTest
+{
 
 	private Strand testedStrand;
 
 	@Before
-	public void setUp() {
-		final BaseFlyweightFactory baseFactory = new BaseFlyweightFactory(Alphabet.DEFAULT_ALPHABET);
-		final Base[] bases = { baseFactory.createGBase(),
-				baseFactory.createABase(), baseFactory.createTBase(),
-				baseFactory.createABase(), baseFactory.createCBase(),
-				baseFactory.createABase() };
-		testedStrand = new ConcreteStrand(bases);
+	public void setUp()
+	{
+		final char[] bases = { 'G', 'A', 'T', 'A', 'C', 'A' };
+		testedStrand = new Strand(bases);
 	}
 
 	@Test
-	public void testComplementary() {
+	public void testComplementary()
+	{
 		final Strand complementaryStrand = testedStrand.getComplementary();
-		assertEquals("Le brin complementaire est-il correct ?", "CTATGT", complementaryStrand.toString());
+		assertEquals("Le brin complementaire est-il correct ?", "CTATGT",
+				complementaryStrand.toString());
 	}
-	
+
 	@Test
-	public void testReverse() {
+	public void testReverse()
+	{
 		final Strand reverseStrand = testedStrand.getReverse();
-		assertEquals("Le brin reverse est-il correct ?", "ACATAG", reverseStrand.toString());
+		assertEquals("Le brin reverse est-il correct ?", "ACATAG",
+				reverseStrand.toString());
 	}
-	
+
 	@Test
-	public void testReverseComplementary() {
-		final Strand reverseComplementaryStrand = testedStrand.getReverseComplementary();
-		assertEquals("Le brin reverse-complementaire est-il correct ?", "TGTATC", reverseComplementaryStrand.toString());
+	public void testReverseComplementary()
+	{
+		final Strand reverseComplementaryStrand = testedStrand
+				.getReverseComplementary();
+		assertEquals("Le brin reverse-complementaire est-il correct ?",
+				"TGTATC", reverseComplementaryStrand.toString());
 	}
-	
+
 	@Test
-	public void testPrefix() {
-		testedStrand = new ConcreteStrand("TACTAGA", Alphabet.DEFAULT_ALPHABET);
+	public void testPrefix()
+	{
+		testedStrand = new Strand("TACTAGA");
 		Strand prefix = testedStrand.getPrefix(6);
 		assertEquals("TACTAG", prefix.toString());
 		assertTrue(testedStrand.isPrefix(prefix));
@@ -70,10 +76,11 @@ public class StrandTest {
 		assertEquals("", prefix.toString());
 		assertFalse(testedStrand.isPrefix(prefix));
 	}
-	
+
 	@Test
-	public void testSuffix() {
-		testedStrand = new ConcreteStrand("TACTAGA", Alphabet.DEFAULT_ALPHABET);
+	public void testSuffix()
+	{
+		testedStrand = new Strand("TACTAGA");
 		Strand suffix = testedStrand.getSuffix(6);
 		assertEquals("ACTAGA", suffix.toString());
 		assertTrue(testedStrand.isSuffix(suffix));
@@ -96,18 +103,19 @@ public class StrandTest {
 		assertEquals("", suffix.toString());
 		assertFalse(testedStrand.isSuffix(suffix));
 	}
-	
+
 	@Test
-	public void testEdge() {
-		testedStrand = new ConcreteStrand("TACAGTA", Alphabet.DEFAULT_ALPHABET);
-		Strand edge = new ConcreteStrand("TA",Alphabet.DEFAULT_ALPHABET);
+	public void testEdge()
+	{
+		testedStrand = new Strand("TACAGTA");
+		Strand edge = new Strand("TA");
 		assertTrue(testedStrand.isEdge(edge));
-		assertFalse(testedStrand.isEdge(new ConcreteStrand("T", Alphabet.DEFAULT_ALPHABET)));
-		assertFalse(testedStrand.isEdge(new ConcreteStrand("A", Alphabet.DEFAULT_ALPHABET)));
-		testedStrand = new ConcreteStrand("TACGAGATAC", Alphabet.DEFAULT_ALPHABET);
+		assertFalse(testedStrand.isEdge(new Strand("T")));
+		assertFalse(testedStrand.isEdge(new Strand("A")));
+		testedStrand = new Strand("TACGAGATAC");
 		edge = testedStrand.getLongestEdge();
 		assertEquals("TAC", edge.toString());
-		testedStrand = new ConcreteStrand("TACGAGA", Alphabet.DEFAULT_ALPHABET);
+		testedStrand = new Strand("TACGAGA");
 		edge = testedStrand.getLongestEdge();
 		assertEquals("", edge.toString());
 	}
