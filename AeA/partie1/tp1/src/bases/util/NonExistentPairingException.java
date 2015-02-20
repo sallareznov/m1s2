10c5ec085d0fc4b6f9273af6e2c3ebaa7b2cd0b2
Salla <diagne14@gmail.com>
2015-02-20 11:46:26 +0100

    [AeA] tp2 : NonExistentPairingException

---
+++ AeA/partie1/tp1/src/bases/util/NonExistentPairingException.java
@@ -0,0 +1,11 @@
+package bases.util;
+
+public class NonExistentPairingException extends Exception {
+
+	private static final long serialVersionUID = 4985973374579137000L;
+	
+	public NonExistentPairingException(char base) {
+		super("No pairing found for : " + base);
+	}
+
+}
