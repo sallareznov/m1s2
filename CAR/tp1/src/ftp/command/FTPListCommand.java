1c633e760dd6a3f795da2045cd480fd8f14895d2
Salla <diagne14@gmail.com>
2015-02-25 15:29:32 +0100

    [CAR] tp1 : amélioration de la commande LIST

--- CAR/tp1/src/ftp/command/FTPListCommand.java
+++ CAR/tp1/src/ftp/command/FTPListCommand.java
@@ -2,6 +2,12 @@ package ftp.command;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import ftp.FTPDatabase;
 import ftp.FTPMessageSender;
@@ -11,10 +17,12 @@ import ftp.configuration.FTPClientConfiguration;
  * Class representing the LIST command
  */
 public class FTPListCommand extends FTPMessageSender implements FTPCommand {
-	
+
 	/**
 	 * constructs a LIST command
-	 * @param database the database
+	 * 
+	 * @param database
+	 *            the database
 	 */
 	public FTPListCommand(FTPDatabase database) {
 		super(database);
@@ -23,6 +31,33 @@ public class FTPListCommand extends FTPMessageSender implements FTPCommand {
 	@Override
 	public boolean accept(String command) {
 		return command.equals("LIST");
+	}
+
+	private String buildListItem(File entry) {
+		try {
+			PosixFileAttributes attr = Files.readAttributes(
+					Paths.get(entry.getAbsolutePath()),
+					PosixFileAttributes.class);
+			char dir = '-';
+			if (entry.isDirectory())
+				dir = 'd';
+			if (Files.isSymbolicLink(entry.toPath()))
+				dir = 'l';
+			String chmod = PosixFilePermissions.toString(Files
+					.getPosixFilePermissions(entry.toPath()));
+			String owner = attr.owner().getName();
+			String group = attr.group().getName();
+			long size = entry.length();
+			String lastModif = new SimpleDateFormat("MMM dd HH:mm")
+					.format(new Date(entry.lastModified()));
+			String filename = entry.getName();
+
+			return String.format("%c%s %s %s %6d %s %s", dir, chmod,
+					owner, group, size, lastModif, filename);
+		} catch (IOException e) {
+			System.err.println("ERROR while getting POSIX attributes");
+			return null;
+		}
 	}
 
 	@Override
@@ -39,13 +74,8 @@ public class FTPListCommand extends FTPMessageSender implements FTPCommand {
 		final File[] directoryListing = workingDirectory.listFiles();
 		final StringBuilder messageBuilder = new StringBuilder();
 		for (final File entry : directoryListing) {
-			if (!entry.isHidden()) {
-				if (entry.isDirectory())
-					messageBuilder.append(entry.getName()
-							+ clientConfiguration.getDirectorySeparator() + "\n");
-				else
-					messageBuilder.append(entry.getName() + "\n");
-			}
+			if (!entry.isHidden())
+				messageBuilder.append(buildListItem(entry) + "\n");
 		}
 		sendData(clientConfiguration.getDataSocket(), messageBuilder.toString());
 		try {
