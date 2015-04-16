ff717179021ded0b2b197d2d5bc737eb5f3b11a7
sallareznov <diagne14@gmail.com>
2015-04-16 11:19:10 +0200

    [SVL] suppression d'un dossier inutile (dossier de démo)

--- SVL/demo_SVL_CTD7_selenium/calcul_temperature.py
+++
@@ -1,5 +0,0 @@
-# SVL 1415 - M. Nebut
-
-# à terminer
-def celsius_to_farhenheit(celsius):
-    return 50
--- SVL/demo_SVL_CTD7_selenium/test_calcul_temperature.py
+++
@@ -1,6 +0,0 @@
-from calcul_temperature import *
-from nose.tools import assert_equal
-
-def test_celsius_to_fahrenheit():
-    assert_equal(celsius_to_farhenheit(10), 50)
-    
