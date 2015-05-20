c556270f12723b0254e9da33385eacda8d74005d
sallareznov <diagne14@gmail.com>
2015-05-20 17:12:45 +0200

    [SVL] tp9 : version plus correcte

--- SVL/tp9/banque.cmd
+++ SVL/tp9/banque.cmd
@@ -3,12 +3,21 @@
 
 !create compte : Compte
 !set compte.solde := 300
+!set compte.plancher := 15
 
 !create personne : Personne
 !set personne.revenu := 15
 
 !create conseiller : Personne
 !set conseiller.revenu := 15
+
+!openter compte debiter(50)
+!set compte.solde := 250
+!opexit
+
+!openter compte crediter(75)
+!set compte.solde := 325
+!opexit
 
 !insert (personne, conseiller) into Conseil
 !insert (conseiller, personne) into Conseil
